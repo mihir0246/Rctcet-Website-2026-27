4859677f7a6f317f54a2f01cd79257bf6a92c653
mihir0246 <mihirsonawane2006@gmail.com>
2026-08-21 03:47:57 +0530

    Fix case-sensitive imports in App.jsx to resolve Vercel build error

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Layout from './layout';
 import { ThemeProvider } from './context/themeContext';
 import { HelmetProvider } from 'react-helmet-async';
 
-const Home = lazy(() => import('./pages/Home'));
+const Home = lazy(() => import('./pages/home'));
 const Achievement = lazy(() => import('./pages/Achievement').then(m => ({ default: m.Achievement })));
-const TeamPage = lazy(() => import('./pages/MeetTheTeam'));
-const About = lazy(() => import('./pages/About'));
-const Projects = lazy(() => import('./pages/Projects'));
+const TeamPage = lazy(() => import('./pages/meetTheTeam'));
+const About = lazy(() => import('./pages/about'));
+const Projects = lazy(() => import('./pages/projects'));
 const RegistrationForm = lazy(() => import('./pages/Registration'));
 const UpcomingEvents = lazy(() => import('./pages/UpcomingEvents'));
 const EventRegistration = lazy(() => import('./pages/EventRegistration'));
