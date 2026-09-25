c108752c7c7fd03a2852ad984f8fe4634818c14b
fuzzu05 <fuzailk05@gmail.com>
2026-09-25 23:49:26 +0530

    lol!

--- src/pages/Admin/HrdDashboard.jsx
+++ src/pages/Admin/HrdDashboard.jsx
@@ -3,7 +3,7 @@ import { useAdminAuth } from '../../context/AdminAuthContext';
 import Navbar from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+const BACKEND_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
 
 const HrdDashboard = () => {
   const { getToken } = useAdminAuth();
